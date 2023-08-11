ba194917db2366cd713214e1e2c94858634cf17d
PhobiaGH <phobiaghat@proton.me>
2023-08-11 08:47:44 -0400

    Fixed code that broke main functionality

--- crawl.js
+++ crawl.js
@@ -5,9 +5,19 @@ async function crawlPage(currentURL) {
     console.log(`Actively crawling: ${currentURL}`)
     try {
         const resp = await fetch(currentURL)
+        if (resp.status > 399) {
+            console.log(`error attempting fetch with status code: ${resp.status} on page: ${currentURL}`)
+            return
+        };
+        const contentType = resp.headers.get("content-type")
+        if (!contentType.includes("text/html")) {
+            console.log(`Non HTML response, content type: ${contentType} on page ${currentURL}`)
+            return
+        };
         console.log(await resp.text())
     } catch (err) {
         console.log(`error attempting fetch: ${err.message}, on page: ${currentURL}`)
+        return
     };
 };
 
