74ce60debd2dcd168f60de1b55d397267b0030f0
PhobiaGH <phobiaghat@proton.me>
2023-08-11 11:01:30 -0400

    Coded print report function

---
+++ report.js
@@ -0,0 +1,31 @@
+//Prints final report
+function printReport(pages) {
+    console.log("========")
+    console.log("REPORT")
+    console.log("========")
+    const sortedPages = sortPages(pages)
+    for (const sortedPage of sortedPages) {
+        const url = sortedPage[0]
+        const hits = sortedPage[1]
+        console.log(`Found ${hits} links to page: ${url}`)
+    };
+    console.log("========")
+    console.log("END REPORT")
+    console.log("========")
+};
+
+// Sorts output for report
+function sortPages(pages) {
+    const pagesArr = Object.entries(pages)
+    pagesArr.sort((a,b) => {
+        aHits = a[1]
+        bHits = b[1]
+        return b[1] - a[1]
+    });
+    return pagesArr
+};
+
+// Exports function for use elsewhere in the app
+module.exports = {
+    sortPages, printReport
+};
---
+++ report.test.js
@@ -0,0 +1,37 @@
+/* This is the test enviorment for Jest, it is used for development purposes only. 
+If you are looking for the meat and bones of the code, you may safely ignore this 
+file unless you are interested in the complete workings of this app. Feel free install 
+Jest on your own machine, and borrow the code below if you are interested in giving Jest a try */
+
+const {sortPages} = require('./report.js');
+const {test,expect} =require('@jest/globals');
+
+// Tests sorting of pages (2 pages)
+test('sortPages 2', () => {
+    const input = {
+        'https://github.com': 1,
+        'https://github.com/PhobiaGH': 3
+    };
+    const actual = sortPages(input)
+    const expected = [
+        ['https://github.com/PhobiaGH', 3],
+        ['https://github.com', 1]
+    ]
+    expect(actual).toEqual(expected)
+});
+
+// Tests sorting of pages (3 pages)
+test('sortPages 3', () => {
+    const input = {
+        'https://github.com': 1,
+        'https://github.com/PhobiaGH': 3,
+        'https://github.com/PhobiaGH/path': 2
+    };
+    const actual = sortPages(input)
+    const expected = [
+        ['https://github.com', 1],
+        ['https://github.com/PhobiaGH', 3],
+        ['https://github.com/PhobiaGH/path', 2]
+    ]
+    expect(actual).toEqual(expected)
+});
